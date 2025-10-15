dd7ebe2a6bdf262b3e400be1c6f2ec4564f3af9d
cahern <mon1_9@hotmail.com>
2025-10-14 18:37:54 -0600

    changes on index and timeline - add LineController and BarController to imports

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,28 +1,9 @@
 import React, { useRef, useEffect } from 'react';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
+
 import { Chart } from 'react-chartjs-2';
 import './Timeline.css';
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
+
 
 const Timeline = ({ data, currentMonth, totalMonths, onMonthChange, config }) => {
   const chartRef = useRef();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  LineController,
+  BarController  
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  LineController,
+  BarController  
+);
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
